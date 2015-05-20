b4ed8c4337d4c49ffdd1e5c099060f6678dc81d6
Vojta Stavik <vojta@ulikeit.com>
2015-05-20 14:45:16 +0200

    Add the random sentece and random picture generators

--- MinionGenerator.swift
+++ MinionGenerator.swift
@@ -10,8 +10,6 @@ import Foundation
 
 struct MinionGenerator: GeneratorType {
     
-    typealias Element = Minion
-    
     class Minion: Equatable {
 
         let name: String
@@ -22,26 +20,20 @@ struct MinionGenerator: GeneratorType {
         init?(name: String) {
             
             self.name = name
-
-            let filePath = __FILE__
-            
-            let currentDirectoryPath = filePath.substringToIndex(advance(filePath.endIndex, -count("MinionGenerator.swift")))
+            self.profilePictureURL = MinionGenerator.urlForPicture(name)
             
             if let
                 
-                profilePictureData = NSData(contentsOfFile: currentDirectoryPath + "images/" + name + ".jpg"),
-                profilePicture = UIImage(data: profilePictureData, scale: UIScreen.mainScreen().scale),
-                profilePictureURL = NSURL(string: "https://raw.githubusercontent.com/VojtaStavik/MinionGenerator/master/images/" + name + ".jpg")
+                profilePictureData = NSData(contentsOfFile: MinionGenerator.pathForLocalPicture(name)),
+                profilePicture = UIImage(data: profilePictureData, scale: UIScreen.mainScreen().scale)
             {
                 
                 self.profilePicture = profilePicture
-                self.profilePictureURL = profilePictureURL
             }
             
             else {
                 
                 profilePicture = UIImage()
-                profilePictureURL = NSURL()
                 
                 return nil
             }
@@ -49,12 +41,18 @@ struct MinionGenerator: GeneratorType {
     }
     
     
+    // GeneratorType protocol
+    
+    typealias Element = Minion
     
     func next() -> Element? {
         
         return self.dynamicType.randomMinion()
     }
     
+    
+    
+    // Random minions
     
     static func randomMinion() -> Minion {
         
@@ -64,7 +62,7 @@ struct MinionGenerator: GeneratorType {
     
     static func randomMinionName() -> String {
         
-        return knownMinions.randomItem()!
+        return knownMinions.randomItem()
     }
     
     
@@ -77,13 +75,53 @@ struct MinionGenerator: GeneratorType {
     }
     
     
-    static func paragraph(sentences: Int) -> String {
+    
+    // Other assets
+    
+    static func randomSentence() -> String {
+        
+        return randomParagraph(1)
+    }
+    
+    
+    static func randomParagraph(sentences: Int) -> String {
         
         return ". ".join(createSmartArray(sentences, randomElement: { () -> String in
             
-            return self.sourceTextArray.randomItem()!
+            return self.sourceTextArray.randomItem()
         }))
     }
+    
+    
+    static func randomPicture() -> UIImage {
+        
+        return UIImage(data: NSData(contentsOfFile: pathForLocalPicture(randomImageNames.randomItem()))!)!
+    }
+    
+    
+    static func randomPrictureURL() -> NSURL {
+        
+        return urlForPicture(randomImageNames.randomItem())
+    }
+    
+    
+    
+    // MARK: - Private functions
+    
+    private static func pathForLocalPicture(name: String) -> String {
+        
+        let filePath = __FILE__
+        let currentDirectoryPath = filePath.substringToIndex(advance(filePath.endIndex, -count("MinionGenerator.swift")))
+
+        return currentDirectoryPath + "images/" + name + ".jpg"
+    }
+
+    
+    private static func urlForPicture(name: String) -> NSURL {
+        
+        return NSURL(string: "https://raw.githubusercontent.com/VojtaStavik/MinionGenerator/master/images/" + name + ".jpg")!
+    }
+    
     
     
     // Returns an array of elements where two similar elements are not next to each other
@@ -117,6 +155,7 @@ struct MinionGenerator: GeneratorType {
         return array
     }
     
+    
     // MARK: - Data sources
     
     static private let knownMinions = [
@@ -137,6 +176,15 @@ struct MinionGenerator: GeneratorType {
     
     static private let sourceTextArray = "Evolving from single-celled yellow organisms at the dawn of time, Minions live to serve, but find themselves working for a continual series of unsuccessful masters, from T. Rex to Napoleon. Without a master to grovel for, the Minions fall into a deep depression. But one minion, Kevin, has a plan; accompanied by his pals Stuart and Bob, Kevin sets forth to find a new evil boss for his brethren to follow. Their search leads them to Scarlet Overkill, the world's first-ever super-villainess. A man who delights in all things wicked, supervillain Gru hatches a plan to steal the moon. Surrounded by an army of little yellow minions and his impenetrable arsenal of weapons and war machines, Gru makes ready to vanquish all who stand in his way. But nothing in his calculations and groundwork has prepared him for his greatest challenge: three adorable orphan girls who want to make him their dad. Now that Gru has forsaken a life of crime to raise Margo, Agnes and Edith, he's trying to figure out how to provide for his new family. As he struggles with his responsibilities as a father, the Anti-Villain League -- an organization dedicated to fighting evil -- comes calling. The AVL sends Gru on a mission to capture the perpetrator of a spectacular heist, for who would be better than the world's greatest ex-villain to capture the individual who seeks to usurp his power".componentsSeparatedByString(". ")
     
+    static private let randomImageNames = [
+    
+        "random1",
+        "random2",
+        "random3",
+        "random4",
+        "random5"
+    ]
+    
 }
 
 
@@ -147,12 +195,7 @@ func ==<T: MinionGenerator.Minion> (lhs:T, rhs:T) -> Bool {
 
 extension Array {
     
-    func randomItem() -> T? {
-        
-        if self.count == 0 {
-            
-            return nil
-        }
+    func randomItem() -> T {
         
         let index = Int(arc4random_uniform(UInt32(self.count)))
         return self[index]
